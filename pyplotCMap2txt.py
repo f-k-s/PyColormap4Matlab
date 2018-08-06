fcc877dc882687243b8b57f8d6b727ec715abdfd
f-k-s <40491902+f-k-s@users.noreply.github.com>
2018-08-06 17:47:35 +0200

    retrieve colormap names from python directly
    ...instead of hard-coding of colormap names.

--- pyplotCMap2txt.py
+++ pyplotCMap2txt.py
@@ -1,3 +1,7 @@
+# Write matplotlib colormaps to ascii file.
+# Retrieve colormap names with argument 'listCMapNames'
+# Version 1.1; K. Schumacher 08.2018
+
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
@@ -8,13 +12,11 @@ def get_cmap(name, nVal):
     return cmp(range(nVal))
 
 def write_cmap(file, cmap):
-#    with open(file, 'w') as f:
-#        f.write('%d' % cmap)
     np.savetxt(file, cmap)
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
-    ap.add_argument('cmName', type=str, choices=plt.colormaps())
+    ap.add_argument('cmName', type=str, choices=['listCMapNames']+plt.colormaps())
     ap.add_argument('-o', type=str, dest='outfile')
     ap.add_argument('-n', type=int, default=256, dest='nVal')
     
@@ -23,7 +25,10 @@ if __name__ == '__main__':
     if args.outfile is None:
         args.outfile = args.cmName + '.txt'
 
-    cmp = get_cmap(args.cmName, args.nVal)
-
-    write_cmap(args.outfile, cmp)
+    if args.cmName in 'listCMapNames':
+        with open(args.outfile,'w') as f:
+            f.writelines(c+'\n' for c in plt.colormaps())
+    else:
+        cmp = get_cmap(args.cmName, args.nVal)
+        write_cmap(args.outfile, cmp)
     
